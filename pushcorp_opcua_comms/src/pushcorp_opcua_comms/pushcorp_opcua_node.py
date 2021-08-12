fdf1f233155c739bc409ba7144dee433f5a0fe9f
Doug Smith <douglas.smith@swri.org>
2021-08-12 13:49:23 -0500

    Automatically create services and handlers for inputs

--- pushcorp_opcua_comms/src/pushcorp_opcua_comms/pushcorp_opcua_node.py
+++ pushcorp_opcua_comms/src/pushcorp_opcua_comms/pushcorp_opcua_node.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import asyncio
 
+from typing import Optional
 import aiorospy
 import rospy
 import sys
@@ -8,9 +9,12 @@ import copy
 import traceback
 import pushcorp_msgs
 import ros_opcua_srvs.srv
+import std_srvs.srv
 from rospy import ServiceProxy
-from std_msgs.msg import String
+from std_msgs.msg import String, Float32, Bool, Int32
 from std_srvs.srv import SetBool, SetBoolRequest, SetBoolResponse
+import pushcorp_msgs.srv
+from enum import IntEnum
 
 from asyncua import Client, ua, Node
 from asyncua.common.subscription import Subscription
@@ -21,68 +25,143 @@ OPCUA_SPINDLE = 'ns=4;s=GVL_opcua.stSpindleControl'
 
 
 class FcuData:
-    def __init__(self, client: Client):
 
+    # noinspection PyTypeChecker
+    def __init__(self, client: Client, node_uri: str, ns: str):
+
+        self.ns = ns
+        self.node_uri = node_uri
         self.event_loop = asyncio.get_running_loop()
-        self.client = client
-        self.st_data = None
-        self.hb_timer = None
+        self.client: Client = client
 
         self.Heartbeat: Node = None
+
+        self.st_data = None  # Full data (hb, input, output)
         self.st_data_node: Node = None
+        self.st_data_input_node: Node = None
         self.sub_opcua: Subscription = None
-        self.hb = None
+        self.lock = asyncio.Lock()
+        self.subs = []
+        self.srvs = []
 
     async def map_data(self):
-        print(OPCUA_FCU)
+        print(self.node_uri)
 
-        self.st_data_node = self.client.get_node(OPCUA_FCU)
+        self.st_data_node = self.client.get_node(self.node_uri)
         self.st_data = await self.st_data_node.get_value()
+        self.st_data_input_node = self.client.get_node(self.node_uri + '.input')
 
-        self.Heartbeat = self.client.get_node(OPCUA_FCU + '.Heartbeat')
+        self.Heartbeat = self.client.get_node(self.node_uri + '.Heartbeat')
 
         self.sub_opcua = await self.client.create_subscription(5, self)
 
         await self.sub_opcua.subscribe_data_change(self.st_data_node)
         self.event_loop.create_task(self.heartbeat_co())
 
+        self.st_data = await self.st_data_node.get_value()
+
+        in_vars = vars(self.st_data.input)
+        for key, val in in_vars.items():
+            sub = None
+            topic_name = self.ns + '/' + str(key)
+            if type(val) is float:
+                sub = aiorospy.AsyncSubscriber(topic_name, Float32)
+            elif type(val) is int or isinstance(val, IntEnum):
+                sub = aiorospy.AsyncSubscriber(topic_name, Int32)
+            elif type(val) is bool:
+                sub = aiorospy.AsyncSubscriber(topic_name, Bool)
+            else:
+                print(f'Unknown type {key}, {type(val)} in structure')
+
+            if sub is not None:
+                self.subs.append(sub)
+
+        for key, val in in_vars.items():
+            srv = None
+            topic_name = self.ns + '/' + str(key)
+            if type(val) is float:
+                srv = aiorospy.AsyncService(topic_name, pushcorp_msgs.srv.SetFloat32,
+                                            lambda req: self.svc_handler(req=req, name=key,
+                                                                         ret_type=pushcorp_msgs.srv.SetFloat32Response))
+            elif type(val) is int or isinstance(val, IntEnum):
+                srv = aiorospy.AsyncService(topic_name, pushcorp_msgs.srv.SetInt32,
+                                            lambda req: self.svc_handler(req=req, name=key,
+                                                                         ret_type=pushcorp_msgs.srv.SetInt32Response))
+            elif type(val) is bool:
+                srv = aiorospy.AsyncService(topic_name, std_srvs.srv.SetBool,
+                                            lambda req: self.svc_handler(req=req, name=key,
+                                                                         ret_type=std_srvs.srv.SetBoolResponse))
+            else:
+                print(f'Unknown type {key}, {type(val)} in structure')
+
+            if srv is not None:
+                self.srvs.append(srv)
+
+        # asyncio.create_task(self.topic_listeners_init())
+        asyncio.create_task(self.svc_handlers_init())
+
+    async def svc_handler(self, req, name, ret_type):
+        await self.set_named_val(name, req.data)
+        return ret_type(success=True)
+
+
+    async def svc_handlers_init(self):
+        await asyncio.gather(*[srv.start() for srv in self.srvs])
+
+    async def topic_listener(self, sub):
+        async for msg in sub.subscribe():
+            print(f'{sub.name} Heard {msg.data}')
+            await self.set_named_val(sub.name, msg.data)
+
+    async def topic_listeners_init(self):
+        await asyncio.gather(*[self.topic_listener(sub) for sub in self.subs])
+
     async def datachange_notification(self, node, val, data):
         if node == self.st_data_node:
             self.st_data = val
 
-        print(f'Datachange {self.st_data.Heartbeat}')
-
     async def heartbeat_co(self):
-        while(True):
-            val = self.st_data.Heartbeat
+        while True:
+            val = not self.st_data.Heartbeat
             print(val)
-            await self.Heartbeat.set_value(ua.DataValue(not val))
+            try:
+                await self.Heartbeat.set_value(ua.DataValue(val))
+            except asyncio.exceptions.TimeoutError:
+                rospy.logerr(f"{self.ns} Heartbeat to opcua timed out")
+                raise
             await asyncio.sleep(1)
 
+    def get_input_vals(self):
+        return self.st_data.input
+
+    async def set_input_vals(self, data):
+        async with self.lock:
+            await self.st_data_input_node.write_value(ua.DataValue(data))
+
+    def get_output_vals(self):
+        return self.st_data.output
+
+    async def set_named_val(self, name: str, value: any):
+
+        vals_current = self.st_data.input
+        v = vars(vals_current)
+        setattr(vals_current, name, value)
+        try:
+            await self.set_input_vals(vals_current)
+        except:
+            traceback.print_exc()
 
 class PushcorpComms:
-    opcua_srv_connect: ServiceProxy
-    opcua_srv_disconnect: ServiceProxy
 
-    def datachange_notification(self, node, val, data):
-        print("Python: New data change event", node, val)
+    # def event_notification(self, event):
+    #     print("Python: New event", event)
 
-        if node == self.st_fcu_node:
-            print('fcu node update')
-            self.st_fcu = val
-        elif node == self.st_spindle_node:
-            print('spindle node update')
-            self.st_spindle = val
-
-    def event_notification(self, event):
-        print("Python: New event", event)
-
+    # noinspection PyTypeChecker
     def __init__(self, opcua_endpoint: str):
         self.opcua_endpoint = opcua_endpoint
 
         self.event_loop = None
         self.client: Client = None
-        self.objects: Node = None
         self.st_fcu_node: Node = None
         self.st_spindle_node: Node = None
         self.st_fcu = None
@@ -93,8 +172,7 @@ class PushcorpComms:
         self.st_fcu_input_node: Node = None
 
         self.fcu_data: FcuData = None
-
-    #        rospy.Timer(rospy.Duration(1.0), self.heartbeat_cb)
+        self.spindle_data: FcuData = None
 
     async def setbool_svc(self, req: SetBoolRequest) -> SetBoolResponse:
         resp = SetBoolResponse()
@@ -103,9 +181,11 @@ class PushcorpComms:
         return resp
 
     async def listener(self):
-        sub = aiorospy.AsyncSubscriber('blah', String)
+
+        sub = aiorospy.AsyncSubscriber('~/blah', String)
+
         async for message in sub.subscribe():
-            print(f'I heard: {message.data}')
+            await self.fcu_data.set_named_val('SetForce', 15)
 
     async def connect(self):
         self.event_loop = asyncio.get_running_loop()
@@ -115,30 +195,20 @@ class PushcorpComms:
             print('connected')
             await self.client.load_data_type_definitions()
 
-            self.st_fcu_node = self.client.get_node(OPCUA_FCU)
-            self.st_fcu = await self.st_fcu_node.get_value()
-
-            self.st_fcu_input_node = self.client.get_node(OPCUA_FCU + '.input')
-
             self.st_spindle_node = self.client.get_node(OPCUA_SPINDLE)
             self.st_spindle = await self.st_spindle_node.get_value()
 
-            # subscribing to a variable node
-
-            # self.sub_opcua = self.client.create_subscription(500, self)
-
-            # self.sub_handle = self.sub_opcua.subscribe_data_change([self.st_fcu_node, self.st_spindle_node])
-
             self.server = aiorospy.AsyncService('service', SetBool, self.setbool_svc)
 
-            print(self.st_fcu.input.SetForce)
+            self.fcu_data = FcuData(self.client, OPCUA_FCU, '/pushcorp/fcu')
 
-            self.fcu_data = FcuData(self.client)
+            self.spindle_data = FcuData(self.client, OPCUA_SPINDLE, '/pushcorp/spindle')
+
+
             await asyncio.gather(self.listener(),
                                  self.fcu_data.map_data(),
+                                 self.spindle_data.map_data(),
                                  self.server.start())
-            print('safsafs')
-            #await self.fcu_data.map_data()
 
         except asyncio.CancelledError:
             raise
@@ -150,33 +220,8 @@ class PushcorpComms:
     async def run(self):
         await self.connect()
 
-
-
     def disconnect(self):
         self.client.disconnect()
-
-    def init_ros(self, opcua_endpoint: str):
-        self.opcua_endpoint = opcua_endpoint
-
-        self.opcua_srv_disconnect = rospy.ServiceProxy('/opcua/opcua_client/disconnect', ros_opcua_srvs.srv.Disconnect)
-        self.opcua_srv_connect = rospy.ServiceProxy('/opcua/opcua_client/connect', ros_opcua_srvs.srv.Connect)
-
-        rospy.loginfo('Calling disconnect on startup')
-        dc_resp = self.opcua_srv_disconnect()
-
-        # Connect to plc
-        rospy.loginfo(f'Calling connect to {opcua_endpoint}')
-
-        resp: ros_opcua_srvs.srv.ConnectResponse = self.opcua_srv_connect(self.opcua_endpoint)
-        if not resp.success:
-            rospy.logerr(f'Unable to connect to opcua endpoint: {resp.error_message}')
-        else:
-            rospy.loginfo('Connected to endpoint')
-
-    def destroy(self):
-        print("destroy")
-        if self.opcua_srv_disconnect is not None:
-            self.opcua_srv_disconnect()
 
 
 if __name__ == '__main__':
@@ -185,9 +230,9 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
 
-    opcua_endpoint = ip = rospy.get_param('~opcua_endpoint', "opc.tcp://192.168.125.39:4840")
+    opcua_ep = ip = rospy.get_param('~opcua_endpoint', "opc.tcp://192.168.125.39:4840")
 
-    pc = PushcorpComms(opcua_endpoint)
+    pc = PushcorpComms(opcua_ep)
 
     task = loop.create_task(pc.run())
     aiorospy.cancel_on_exception(task)
@@ -197,4 +242,5 @@ if __name__ == '__main__':
         loop.run_until_complete(task)
     except asyncio.CancelledError:
         pass
-
+    except:
+        traceback.print_exc()
