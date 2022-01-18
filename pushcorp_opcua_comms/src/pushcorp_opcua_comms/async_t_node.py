bb938da82e83d7d17b5f32f67e7c81cce6e04bfa
Doug Smith <douglas.smith@swri.org>
2022-01-18 10:37:45 -0600

    Testing of arrays and speed

--- pushcorp_opcua_comms/src/pushcorp_opcua_comms/async_t_node.py
+++ pushcorp_opcua_comms/src/pushcorp_opcua_comms/async_t_node.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import asyncio
+import random
+
 import aiorospy
 import rospy
 import traceback
@@ -54,7 +56,8 @@ class TestStuff:
                           f'debounce time.')
 
             with Timer():
-                fut = node_do.write_value(ua.DataValue(ua.Variant(Value=set_val)))
+                await node_do.write_value(ua.DataValue(ua.Variant(Value=set_val)))
+
                 di_val = await node_di.read_value()
                 read_cnt = 0
                 while di_val != set_val:
@@ -65,6 +68,26 @@ class TestStuff:
 
             await node_do.write_value(ua.DataValue(ua.Variant(Value=False)))
 
+            array_node = self.client.get_node('ns=4;s=GVL_opcua.stTestBasic.input.arrTest')
+            array_node_data_val = await array_node.read_data_value()
+            blag = array_node_data_val.Value
+
+            variant: ua.Variant = None
+            print('read array')
+            with Timer():
+                variant: ua.Variant = (await array_node.read_data_value()).Value
+
+            print(variant)
+
+            val = variant.Value
+            val = [random.randint(0, 255) for b in val]
+
+            new_variant = ua.Variant(Value=val, VariantType=variant.VariantType, is_array=variant.is_array,
+                                     Dimensions=variant.Dimensions)
+            for i in range(10):
+                with Timer():
+                    await array_node.write_value(ua.DataValue(new_variant))
+            print('')
 
         except:
             traceback.print_exc()
@@ -106,7 +129,7 @@ if __name__ == '__main__':
     rospy.init_node('async_test_node', anonymous=False)
 
     loop = asyncio.get_event_loop()
-    loop.set_debug(True)
+    loop.set_debug(False)
 
     ts = TestStuff("opc.tcp://192.168.125.39:4840")
     # ts = TestStuff("opc.tcp://192.168.72.3:4842")
